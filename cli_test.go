e797ef0c0c947cb0e85aab150a2a1290c58cee76
shiimaxx <yoshima@shiimaxx.com>
2019-01-12 18:47:01 +0900

    Add test for multi rules

--- cli_test.go
+++ cli_test.go
@@ -25,3 +25,27 @@ func TestRun_withConfigFile(t *testing.T) {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
+
+func TestRun_withConfigFile_multiRules(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+
+	var cases = []struct {
+		cmd  string
+		want int
+	}{
+		{cmd: "altercode -c testdata/test_multi.toml -- testdata/bin/test_command warning", want: 254},
+		{cmd: "altercode -c testdata/test_multi.toml -- testdata/bin/test_command deprecated", want: 253},
+		{cmd: "altercode -c testdata/test_multi.toml -- testdata/bin/test_command warning_deprecated", want: 254},
+		{cmd: "altercode -c testdata/test_multi.toml -- testdata/bin/test_command ok", want: 0},
+	}
+
+	for _, c := range cases {
+		t.Run(c.cmd, func(t *testing.T) {
+			args := strings.Split(c.cmd, " ")
+			if got, want := cli.Run(args), c.want; got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+	}
+}
